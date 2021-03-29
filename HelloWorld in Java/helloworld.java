02a153a5dce1aa3c29e97886f2fbbd199f6bd6c1
Ali Faizan <68064523+brownpanthera@users.noreply.github.com>
2021-03-29 16:27:53 +0530

    Add files via upload

---
+++ HelloWorld in Java/helloworld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! World");
+        int num = 07;
+        String name;
+        System.out.println("What is your name");
+        name = sc.nextLine();
+        System.out.println(name + " lucky number " + num);
+        
+    }
+}
